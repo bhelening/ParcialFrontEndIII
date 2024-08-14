57d74a84c987ad59de6e0b477467ad13f9e90b0b
bhelening <bhelenbvgmail.com>
2024-08-14 14:37:43 -0500

    Subida de Proyecto Parcial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Carga de Estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+export default App
